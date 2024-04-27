e6c0d0e91a192bc6f7aca5d9b3d2776e975ec8bb
Daniel Lira <dnllira1@gmail.com>
2024-04-27 18:00:46 -0300

    adicionado novos estilos na tabela

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,25 +19,47 @@ form.addEventListener('submit', function (e) {
   }
 
   const imc = getImc(peso, altura);
-  console.log(imc)
+  const nivelImc = getNivelImc(imc)
+
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`
+
+  setResultado(msg, true)
 });
 
-function getImc (peso, altura) {
+
+function getNivelImc(imc) {
+  const nivel = ['MAGREZA', 'NORMAL', 'SOBREPESO I', 'OBESIDADE II', 'OBESIDADE GRAVE III']
+
+  if (imc >= 39.9) return nivel[4]
+  if (imc >= 29.9) return nivel[3]
+  if (imc >= 24.9) return nivel[2]
+  if (imc >= 18.5) return nivel[1]
+  if (imc < 18.5) return nivel[0]
+}
+
+
+function getImc(peso, altura) {
   const imc = peso / altura ** 2;
   return imc.toFixed(2);
 }
 
 // Função únicamente para criar um paragrafo
-function criaP () {
+function criaP() {
   const p = document.createElement('p');
   return p;
 }
 
-function setResultado (msg, isValid) {
+function setResultado(msg, isValid) {
   const resultado = document.querySelector('#resultado');
   resultado.innerHTML = '';
 
   const p = criaP();
+
+  // if (isValid) {
+  //   p.classList.add('')
+  // } else {
+  //   p.classList.add('')
+  // }
   p.innerHTML = msg;
   resultado.appendChild(p);
 
