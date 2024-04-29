269d6f3274ff5c6554987a7714d25c6986a480d9
Daniel Lira <dnllira1@gmail.com>
2024-04-28 21:02:30 -0300

    finalizado  e

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,3 +67,4 @@ function setResultado(msg, isValid) {
 }
 
 setResultado()
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+      },
+    },
   },
   plugins: [],
 }
